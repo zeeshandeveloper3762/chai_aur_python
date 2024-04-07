7fb191ecd2a6628abdbc26597be988d56b202d7a
zk3762100@gmail.com <132770259+zeeshandeveloper3762@users.noreply.github.com>
2024-04-07 18:13:17 +0500

    07_solution completed

---
+++ chai_aur_python/02_conditionals/07_solution.py
@@ -0,0 +1,11 @@
+# Problem: Customize a coffee order: "Small", "Medium", or "Large" with an option for "Extra shot" of espresso.
+
+order_size = str(input("which kind of coffee you drink?"))
+extra_shot = False
+
+if extra_shot:
+    coffee = order_size + "coffee with an extra shot"
+else:
+    coffee = order_size + " coffee"
+
+print("Order", coffee)
